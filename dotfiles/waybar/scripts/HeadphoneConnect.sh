82710c3e50a081c7fa49cf12edb46e2527add47f
MicroWabin <mateus.rea.t@gmail.com>
2026-06-16 22:21:18 -0600

    Update HeadphoneConnect.sh

--- dotfiles/waybar/scripts/HeadphoneConnect.sh
+++ dotfiles/waybar/scripts/HeadphoneConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-status=$(bluetoothctl info 3C:B0:ED:E2:CF:29 | grep "Connected:")
+status=$(bluetoothctl info {add MAC here} | grep "Connected:")
 
 if [ "$status" == "	Connected: no" ]
 then
